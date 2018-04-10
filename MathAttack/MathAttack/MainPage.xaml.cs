f2a3d4d0668fb34b24debdfd1aeff77882cb035f
TheMattHatter <G00338622@gmit.ie>
2018-04-10 20:52:42 +0100

    Created a test start screen and loaded it asynchronously at the start of the game

--- MathAttack/MathAttack/MainPage.xaml.cs
+++ MathAttack/MathAttack/MainPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,19 +25,35 @@ namespace MathAttack
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        CanvasBitmap StartScreen;
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        // Adapted from https://microsoft.github.io/Win2D/html/T_Microsoft_Graphics_Canvas_UI_Xaml_CanvasControl.htm
         private void GameCanvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
         {
+            // Calls CreateResourcesAsync Task and ensures could will not execute until the task completes
+            args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
+        }
 
+        async Task CreateResourcesAsync(CanvasControl sender)
+        {
+            // Loads the demo start screen
+            // 'await' suspends the calling method and yields control back to its caller until the awaited task is complete.
+            StartScreen = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/how-to-play.png"));
+            
         }
 
         private void GameCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
         {
+            // Draw the start screen
+            args.DrawingSession.DrawImage(StartScreen);
 
+            // Redraw everything in the draw method (roughly 60fps)
+            GameCanvas.Invalidate();
         }
     }
 }
